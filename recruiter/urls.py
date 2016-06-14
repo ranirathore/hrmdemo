20761f1f3aca5e4572c404210bc193911bd6c142
ranirathore <rani21rathore@gmail.com>
2016-06-14 11:42:08 +0530

    Debugging of URL

--- recruiter/urls.py
+++ recruiter/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^home/$',views.home,name='home'),
     url(r'^$',views.user_login,name='login'),
     url(r'^recruiter/logout/$',views.user_logout,name='logout'),
-    url(r'^candidate_detail/(?P<user_id>[0-9])$',views.user_detail,name='candidate_detail'),
+    url(r'^candidate_detail/(?P<user_id>[0-9]+)$',views.user_detail,name='candidate_detail'),
 ]
 
