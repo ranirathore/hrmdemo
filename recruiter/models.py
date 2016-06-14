896fe664a226446d39800a8bffc57c4b87205f0c
ranirathore <rani21rathore@gmail.com>
2016-06-14 11:52:24 +0530

    Model Improvment, Validation and Field type changes

--- recruiter/models.py
+++ recruiter/models.py
@@ -30,7 +30,7 @@ class Candidate(models.Model):
     pg_passing_year = models.CharField(max_length=4, choices = yearrange, blank = True)
     post_pg_cource = models.CharField(max_length=20,blank = True)
     correct_post_pg = models.CharField(max_length=20,blank = True)
-    
+      
     def __str__(self):             
         return self.name
     
